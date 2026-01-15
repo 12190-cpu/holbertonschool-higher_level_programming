d2cddcb58ef03ed33b3ef5c7b65d372e3aec38cf
12190-cpu <12190@holbertonstudents.com>
2026-01-15 15:56:47 +0100

    print

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming \ language that combines remarkable power with very clear syntax"
 str = str[39:67] + str[109:114] + str[:6]
 print(str)
-
