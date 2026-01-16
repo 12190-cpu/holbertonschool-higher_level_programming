38bf599ba740774e05b7401a2fcc1c51f2b19b31
12190-cpu <12190@holbertonstudents.com>
2026-01-16 20:19:59 +0100

    import modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,8 +2,8 @@
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]  # ignore argv[0] qui est le nom du script
+    argv = sys.argv[1:]
     total = 0
     for num in argv:
-        total += int(num)  # convertir chaque argument en entier et l’ajouter à total
+        total += int(num)
     print(total)
