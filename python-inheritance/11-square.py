314d092a5bb7691fa4b270488ea1ec889f4dec45
12190-cpu <12190@holbertonstudents.com>
2026-02-04 20:56:56 +0100

    inheritance

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module that defines the Square class,
+which inherits from Rectangle.
+Implements size validation, area calculation,
+and custom string representation.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    Validates size, implements area(), and custom __str__().
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with validated size.
+
+        Args:
+            size (int): The size of the square sides.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square
+        in the format: [Square] <width>/<height>
+        """
