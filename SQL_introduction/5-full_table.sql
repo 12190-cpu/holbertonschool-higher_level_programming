3094dff7f10418cd429a9a7b9ed2829db5c51a11
12190-cpu <12190@holbertonstudents.com>
2026-03-06 12:34:08 +0000

    full table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Script qui affiche la structure complète de la table first_table
+-- dans la base de données courante.
+-- On ne peut pas utiliser DESCRIBE ou EXPLAIN, donc on utilise SHOW CREATE TABLE.
+
+SHOW CREATE TABLE first_table;
