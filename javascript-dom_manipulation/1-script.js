7ff83e4ad07bec62742fbd8b217c5f4f5660d597
12190-cpu <12190@holbertonstudents.com>
2026-03-23 12:43:35 +0100

    DOM

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  const redHeader = document.querySelector('#red_header');
+
+  redHeader.addEventListener('click', () => {
+    header.style.color = '#FF0000';
+  });
+});
